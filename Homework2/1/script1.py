f1f4cf4c967dfa8e8af42d06b0497bc53e2b2c54
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-14 21:35:03 +0330

    Signals in HW2 fixed

--- Homework2/1/script1.py
+++ Homework2/1/script1.py
@@ -1,17 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-
-def convolution(a, b, mode='full'):
+def convolution(a, b, mode='same'):
     return np.convolve(a, b, mode=mode)
-
-# Systems
-def system1(t, f0):  # First continues signal
-    return np.cos( (np.pi * f0 * t) / 2 ) ** 2
-
-def system2(n, f0):  # Second distributed signal
-    return np.cos( 2 * np.pi * f0 * n )
-
 
 # Initialize 
 start = -10
@@ -19,19 +10,20 @@ end = 10
 step = 0.001
 
 # Signals
-t = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6]
-x1 = [1, 2, 3, 4, 3, 2, 1, 0, -1]
-x2 = [1, 3, 5, 0, 2]
+t = [x for x in range(start, end)]
 
-f, plt_array = plt.subplots(3) 
+x1 = np.zeros(len(t))
+x1[len(x1) / 2 - 2: len(x1) / 2 + 3] = [1, 2, 3, 2, 1]
 
-f.suptitle('Signals convolution')
-
-plt_array[0].stem(t[2:-2], x1) 
-plt_array[1].stem(t[4:-4], x2)
+x2 = np.zeros(len(t))
+x2[len(x2) / 2 - 1: len(x2) / 2 + 2] = [2, 4, -2]
 
 x3 = convolution(x1, x2)
 
-plt_array[2].stem(t, x3)
+f, plot_array = plt.subplots(3)
+
+plot_array[0].stem(t, x1)
+plot_array[1].stem(t, x2)
+plot_array[2].stem(t, x3)
 
 plt.show()
