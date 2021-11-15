fcca305cb4a76586ba1dd36a290e94eccc0e8145
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-15 15:35:55 +0330

    Convolution method implemented

--- Homework2/1/script1.py
+++ Homework2/1/script1.py
@@ -1,22 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-def convolution(a, b, mode='same'):
-    return np.convolve(a, b, mode=mode)
+def convolution(a, b):
+    a = np.concatenate([ np.zeros( len(b) / 2 - 1 ), a,  np.zeros( len(b) / 2 )])
+    b = b[::-1]
+
+    step = len(b)
+    c = np.zeros(len(a) - step + 1)
+
+    for i in range(len(c)):
+        c[i] = np.sum(a[i:i+step] * b)
+    
+    return c
 
 # Initialize 
+
+# CT
 start = -10
 end = 10 
-step = 0.001
+t_step = 0.1
+
+# DT
+d_step = 1
 
 # Signals
-t = [x for x in range(start, end)]
+t = np.arange(start, end, d_step)
 
-x1 = np.zeros(len(t))
-x1[len(x1) / 2 - 2: len(x1) / 2 + 3] = [1, 2, 3, 2, 1]
+x1 = [1, 2, 3, 2, 1, 2, 3]
+x1 = np.concatenate([np.zeros(len(t) / 2 - len(x1) / 2 - 1), x1, np.zeros(len(t) / 2 - len (x1) / 2)])
 
-x2 = np.zeros(len(t))
-x2[len(x2) / 2 - 1: len(x2) / 2 + 2] = [2, 4, -2]
+x2 = [2, 4, -2]
+x2 = np.concatenate([np.zeros(len(t) / 2 - len(x2) / 2 - 1), x2, np.zeros(len(t) / 2 - len (x2) / 2)])
 
 x3 = convolution(x1, x2)
 
