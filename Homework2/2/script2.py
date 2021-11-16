4c6cef123eddd772d364286985c83e98788e1f10
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-16 10:07:08 +0330

    Plosts replace in Homework 2

--- Homework2/2/script2.py
+++ Homework2/2/script2.py
@@ -41,21 +41,23 @@ h2 = 3 * unit_impulse(len(t), 5) - unit_impulse(len(t))
 x3 = convolution(x, h1)
 x4 = convolution(x, h2)
 
-f, plot_array = plt.subplots(5, constrained_layout=True)
+f, plot_array = plt.subplots(3,2, constrained_layout=True)
 
-plot_array[0].stem(t, x)
-plot_array[0].title.set_text('x[t]   = e^2t (-u(t - 2) + u(t + 3))')
+f.delaxes(plot_array[0][1])
 
-plot_array[1].stem(t, h1)
-plot_array[1].title.set_text('h1[t]  = u(n + 10) - u(n)')
+plot_array[0][0].stem(t, x)
+plot_array[0][0].title.set_text('x[t]   = e^2t (-u(t - 2) + u(t + 3))')
 
-plot_array[2].stem(t, x3)
-plot_array[2].title.set_text('y1[t]  = x[t] * h1[t]')
+plot_array[1][0].stem(t, h1)
+plot_array[1][0].title.set_text('h1[t]  = u(n + 10) - u(n)')
 
-plot_array[3].stem(t, h2)
-plot_array[3].title.set_text('h2[t]  = 3d[t - 5] - d[t]')
+plot_array[1][1].stem(t, x3)
+plot_array[1][1].title.set_text('y1[t]  = x[t] * h1[t]')
 
-plot_array[4].stem(t, x4)
-plot_array[4].title.set_text('y2[t]  = x[t] * h1[t]')
+plot_array[2][0].stem(t, h2)
+plot_array[2][0].title.set_text('h2[t]  = 3d[t - 5] - d[t]')
+
+plot_array[2][1].stem(t, x4)
+plot_array[2][1].title.set_text('y2[t]  = x[t] * h1[t]')
 
 plt.show()
