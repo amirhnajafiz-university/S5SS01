5ab376a1b6e3fede53b19d4ad7cc26c5b10e6ad6
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-29 21:17:22 +0330

    Homework 3 is done

--- Homework3/script.py
+++ Homework3/script.py
@@ -11,24 +11,24 @@ def integ(x, start, end):
     return np.sum(x) * step
 
 #a(k)
-def a(sig, k, T0):
+def a(sig, k, T0, t):
     w0 = 2*np.pi/T0
     x = sig * np.cos(k*w0*t)
     return (2/T0) * integ(x, -T0/2, T0/2)
 
 #b(k)
-def b(sig, k, T0):
+def b(sig, k, T0, t):
     w0 = 2*np.pi/T0
     x = sig * np.sin(k*w0*t)
     return (2/T0) * integ(x, -T0/2, T0/2)
 
 #Fourier Series
-def fs(sig, c, T0):
+def fs(sig, c, T0, t):
     w0 = 2*np.pi/T0
     y = np.zeros((len(t), ))
-    y += a(sig, 0, T0)/2
+    y += a(sig, 0, T0, t)/2
     for k in range(1, c + 1):
-        y += a(sig, k, T0)* np.cos(k*w0*t) + b(sig, k, T0) * np.sin(k*w0*t)
+        y += a(sig, k, T0, t)* np.cos(k*w0*t) + b(sig, k, T0, t) * np.sin(k*w0*t)
     return y
 
 #sig1
@@ -42,15 +42,15 @@ x1 += [linear_draw(1, -2, x) for x in np.arange(1, 2, step)]
 
 fig, s1 = plt.subplots()
 s1.plot(t1, x1, 'r', label="x1(t)")
-# for c in range(0, 11):
-#     if(c % 2 ==0):
-#         color = 'g'
-#     else:
-#         color = 'b'
-#     s1.plot(t1, fs(x1, c, 6), color, label = f'C = {c}')
-# s1.set(xlabel = 't', title = '(x1)')
-# s1.legend()
-# s1.grid()
+for c in range(0, 11):
+    if(c % 2 ==0):
+        color = 'g'
+    else:
+        color = 'b'
+    s1.plot(t1, fs(x1, c, 6, t1), color, label = f'C = {c}')
+s1.set(xlabel = 't', title = '(x1)')
+s1.legend()
+s1.grid()
 
 # #sig2
 x2 = [0 for x in np.arange(-4, -3, step)]
@@ -60,14 +60,14 @@ x2 += [1 for x in np.arange(3, 4, step)]
 
 fig, s2 = plt.subplots()
 s2.plot(t2, x2, 'r', label="x2(t)")
-# for c in range(0, 11):
-#     if(c % 2 ==0):
-#         color = 'g'
-#     else:
-#         color = 'b'
-#     s2.plot(t, fs(x2, c, 6), color, label = f'C = {c}')
-# s2.set(xlabel = 't', title = '(x2)')
-# s2.legend()
-# s2.grid()
+for c in range(0, 11):
+    if(c % 2 ==0):
+        color = 'g'
+    else:
+        color = 'b'
+    s2.plot(t2, fs(x2, c, 6, t2), color, label = f'C = {c}')
+s2.set(xlabel = 't', title = '(x2)')
+s2.legend()
+s2.grid()
 
 plt.show()
