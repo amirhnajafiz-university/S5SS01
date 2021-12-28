fbf79db43d022007cec065287c735fa441b50166
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-28 22:02:31 +0330

    HW4 DSB-AM init

---
+++ Homework4/part1/script.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+T0 = 0.1
+FC = 250
+
+
+# Signal 
+def m(t):
+    if t >= 0 and t <= T0:
+        return np.sinc(100 * t)
+    else:
+        return 0
+
+
+# Carrier
+def c(t):
+    return np.cos(2 * np.pi * FC * t)
