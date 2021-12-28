247d23687ee2535cbb050a7fbbc585ed4fdde269
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-28 22:21:59 +0330

    HW4 part 1.1 done

--- Homework4/part1/script.py
+++ Homework4/part1/script.py
@@ -19,3 +19,18 @@ def c(t):
 # DSB-AM signal
 def am(t):
     return ( 1 + m(t) ) * c(t)
+
+
+# Time 
+time = np.arange(0, T0, TS)
+
+# System m
+y1 = [m(x) for x in time]
+# System am
+y2 = [am(x) for x in time]
+
+fig, s1 = plt.subplots(2)
+s1[0].plot(time, y1)
+s1[1].plot(time, y2)
+
+plt.show()
