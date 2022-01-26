fad039d844f00531f5fa427c810e2ebe0f162c21
9831065 <najafizadeh21@gmail.com>
2022-01-26 09:45:27 +0330

    reading wave is done

---
+++ Homework5/phase1/script.py
@@ -0,0 +1,18 @@
+from scipy.io import wavfile 
+import matplotlib.pyplot as plt 
+import numpy as np
+
+## Read the wave file
+samplerate, data = wavfile.read('./phase1sample.wav')
+
+## Calculate length
+length = data.shape[0] / samplerate
+
+# Create time 
+t = np.linspace(0., length, data.shape[0])
+
+## Show the signal in plot
+fig, axs = plt.subplots()
+axs.plot(t[1:250], data[1:250])
+
+plt.show()
