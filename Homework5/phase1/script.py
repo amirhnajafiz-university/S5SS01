4a3d1a571c58a6f5e50f0260b67eddb00ebef7b6
9831065 <najafizadeh21@gmail.com>
2022-01-26 09:54:55 +0330

    hw5 phase1 part 2 and 3 is done

--- Homework5/phase1/script.py
+++ Homework5/phase1/script.py
@@ -1,10 +1,15 @@
+from this import d
 from scipy.io import wavfile 
+from scipy.fft import fft
 import matplotlib.pyplot as plt 
 import numpy as np
 
 
 ## Read the wave file
 samplerate, data = wavfile.read('./phase1sample.wav')
+
+## FFT
+data = fft(data)
 
 ## Calculate length
 length = data.shape[0] / samplerate
@@ -14,10 +19,10 @@ t = np.linspace(0., length, data.shape[0])
 
 ## Show the signal in plot
 fig, axs = plt.subplots()
-axs.plot(t[1:250], data[1:250])
+axs.plot(t, data)
 
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Wave Data")
+plt.legend("Fourier transform")
+plt.xlabel("Frequency")
+plt.ylabel("Value")
 
 plt.show()
