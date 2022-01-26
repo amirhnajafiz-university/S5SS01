b4243f9087f37930682c654cc358c46a9d45b99d
9831065 <najafizadeh21@gmail.com>
2022-01-26 09:48:08 +0330

    hw5 phase1 part 1 is done

--- Homework5/phase1/script.py
+++ Homework5/phase1/script.py
@@ -2,6 +2,7 @@ from scipy.io import wavfile
 import matplotlib.pyplot as plt 
 import numpy as np
 
+
 ## Read the wave file
 samplerate, data = wavfile.read('./phase1sample.wav')
 
@@ -14,5 +15,9 @@ t = np.linspace(0., length, data.shape[0])
 ## Show the signal in plot
 fig, axs = plt.subplots()
 axs.plot(t[1:250], data[1:250])
+
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Wave Data")
 
 plt.show()
